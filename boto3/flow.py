b99bd2b19bef2608a6c2da9d795fed5a5dcad126
erickkendall <ekendall0013@gmail.com>
2020-04-02 07:12:39 -0500

    latest

--- boto3/flow.py
+++ boto3/flow.py
@@ -4,24 +4,26 @@ import re
 session = boto3.Session(profile_name='famc-legacy')
 client = session.client('logs', region_name='us-east-1')
 
-response=client.describe_log_groups()
+response = client.describe_log_groups()
 
+file1 = open("myfile.txt", "w") 
 all_logs = {}
 for log in response.get('logGroups'):
-  log_group_name = log.get('logGroupName') 
-  if 'cfg' and 'vpc' in log_group_name:
-    flowlog_list = []
-    all_streams = []
-    stream_batch = client.describe_log_streams(logGroupName=log_group_name)
-    all_streams += stream_batch['logStreams']
-    while 'nextToken' in stream_batch:
-      stream_batch = client.describe_log_streams(logGroupName=log_group_name,nextToken=stream_batch['nextToken'])
-      all_streams += stream_batch['logStreams']
-    for row in all_streams:
-      flowlog_list.append(row.get('logStreamName'))
-    all_logs.update({log_group_name:flowlog_list})
+    log_group_name = log.get('logGroupName')
+    if 'cfg' and 'vpc' in log_group_name:
+        flowlog_list = []
+        all_streams = []
+        stream_batch = client.describe_log_streams(logGroupName=log_group_name)
+        all_streams += stream_batch['logStreams']
+        while 'nextToken' in stream_batch:
+            stream_batch = client.describe_log_streams(
+                logGroupName=log_group_name, nextToken=stream_batch['nextToken'])
+            all_streams += stream_batch['logStreams']
+        for row in all_streams:
+            flowlog_list.append(row.get('logStreamName'))
+        all_logs.update({log_group_name: flowlog_list})
 
-# all interfaces attached to an EC2 
+# all interfaces attached to an EC2
 all_server_interfaces = {}
 interfacedict = {}
 server_list = session.resource('ec2', region_name='us-east-1')
@@ -36,12 +38,31 @@ for server in server_list.instances.all():
                     interfacelist.append(interface.id)
         interfacedict.update({hostname: interfacelist})
 
-for key in interfacedict.keys():
-  interface = interfacedict.get(key)
-  if len(interface) > 0:
-    print(f"{key} {interface}")
+all_my_logs_to_parse = {}
+for server_name in interfacedict.keys():
+    interface = interfacedict.get(server_name)
+    if len(interface) > 0:
+        for inter in interface:
+            inter = inter + '-all'
+            key_list = list(all_logs.keys())
+            for vpc in key_list:
+                if inter in all_logs.get(vpc):
+                    response = client.get_log_events(
+                        logGroupName=vpc, logStreamName=inter)
+                    ip = []
+                    port = []
+                    srcip_port = []
+                    for i in response.get('events'):
+                        entry = i.get('message')
+                        if len(entry.split()) == 14:
+                            ver, acct, interface, srcaddr, dstaddr, srcport, dstport, protocol, numpackets, numbytes, starttime, endtime, action, logstatus = entry.split()
+                            val = srcaddr + ':' + srcport
+                            if val not in srcip_port:
+                              srcip_port.append(val)
+                    for i in srcip_port:
+                      ip,port = i.split(':')
+                      if len(port) < 5 and '-' not in port:
+                        file1.write(f"{server_name};{ip};{port}\n")
 
 
-key_list = list(all_logs.keys())
-for row in key_list:
-  print(len(all_logs.get(row)))
+file1.close()
